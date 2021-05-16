bcc7cbb95e5466f4f19a8ad603f4ea066708cfa6
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-15 19:25:59 -0500

    fixed layout and linked script to html properly

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    
+}
+
+
+
+
+function clockRefresh() {
+    let clock = document.getElementById("currentDay");
+    let DateTime = luxon.DateTime;
+    clock.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+    }
+    
+    
+    let time = setInterval(clockRefresh, 1000);
+
+   
+
